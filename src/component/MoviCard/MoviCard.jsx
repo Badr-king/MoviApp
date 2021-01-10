0ba39bc48ce132a017801e7778a87fa33955c86b
badrking <badr4653549@gmail.com>
2021-01-10 13:08:51 +0200

    first commit

--- src/component/MoviCard/MoviCard.jsx
+++ src/component/MoviCard/MoviCard.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import {Card,Button} from 'react-bootstrap'
-import './MoviCard.css'
+ import './MoviCard.css'
 export default function Movicard(props) {
     const {movi}=props
     return (
-    <div className="cardfooter">
-        <Card style={{ width: '18rem',height:"13rem" }} >
+    
+        <Card style={{ width: '18rem',height:"35rem" }} >
             <Card.Img variant="top" src={movi.posterurl} className="movieimg" />
             <Card.Body  className="body">
                  <Card.Title>{movi.title}</Card.Title>
                  <Card.Text>
                   {movi.description}
                 </Card.Text>
-                <p className="result">{("⭐").repeat(movi.rate)}</p>
-                <Button variant="primary">Trail</Button>
+                <p className="result">{("⭐").repeat(movi.rate)}</p> 
+                 
             </Card.Body>
+            <Card.Footer>
+            
+            <Button variant="primary">Trail</Button> 
+            </Card.Footer>
            
         </Card>
-    </div>
+    
     )
 }
